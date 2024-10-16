d25399354f50f1478f483629c8a7ab2ae970bc95
Arthur Manyonge <arthurmanyonge22@gmail.com>
2024-10-16 11:22:21 +0300

    dev: create basic navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:storemint_flutter/screens/auth/categories.dart';
+import 'package:storemint_flutter/screens/auth/conditions.dart';
 import 'package:storemint_flutter/screens/auth/login.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:storemint_flutter/screens/auth/products.dart';
+import 'package:storemint_flutter/screens/auth/sizes.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +17,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      routes: Routes.getRoutes(),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Storemint Flutter',
+        routes: Routes.getRoutes(),
+        debugShowCheckedModeBanner: false,
+        
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
       ),
     );
   }
@@ -26,6 +35,11 @@ class MyApp extends StatelessWidget {
 class Routes {
   static final Map<String, Widget Function(BuildContext)> routes = {
     '/': (context) => Login(),
+    '/products': (context)=> Products(),
+    '/conditions': (context)=> Conditions(),
+'/categories': (context)=>Categories(),
+'/sizes': (context)=>Sizes()
+
     };
   static Map<String, Widget Function(BuildContext)> getRoutes() {
     return routes;
--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:formify/formify.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:storemint_flutter/utils.dart';
+
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,11 +13,36 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
+  final loginForm = LoginForm();
+  bool isLoading = false;
 
   Future<void> login()async{
-    FocusScope.of(context).unfocus();
-    final email = _formKey.currentState!.
+    isLoading = true;
+  FocusScope.of(context).unfocus();
+    String email = loginForm.getValue('email');
+    String password = loginForm.getValue('password');
+  
+  final body = {
+    "email": email,
+    "password": password
+  };
+  const url = 'https://dripventory.storemint.shop/api/auth/login';
+  final uri = Uri.parse(url);
+  final response = await http.post(uri, body: jsonEncode(body), headers: {'Content-type': 'application/json'});
+  if(response.statusCode == 201){
+    if(mounted){
+      showSnackBar(message: 'Login successful', context: context);
+      Navigator.pushNamed(context, '/products');
+    isLoading= false;
+    }
+    return;
+  }
+  if(mounted){
+    showSnackBar(message: 'There was an error ${response.body.toString()}', isError: true, context: context);
+    isLoading= false;
+  }
+    isLoading= false;
+
   }
   @override
   Widget build(BuildContext context) {
@@ -24,35 +54,74 @@ class _LoginState extends State<Login> {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Form(key: _formKey,child: Padding(
+          Padding(
             padding: EdgeInsets.symmetric(horizontal: 40),
             child: Column(
               children: [
-                TextFormField(
-                  
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ),
+                ...loginForm.getWidgets().map((widget) => Column(
+                  children: [
+                    widget,
+                    SizedBox(height: 20),
+                  ],
+                )).toList(),
                 SizedBox(height: 20,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20,),
-                SizedBox(width: double.infinity, child: MaterialButton(onPressed: (){}, child: Text('Login')))
+                SizedBox(width: double.infinity, child: ElevatedButton(onPressed: login, child: isLoading? const SizedBox(width: 24, height: 24, child: CircularProgressIndicator(strokeWidth: 2, valueColor: AlwaysStoppedAnimation<Color>(Colors.white),)): Text('Login')))
               ],
             ),
-          ),)
+          ),
         ],
       ),
     );
   }
+}
+
+class LoginForm extends FormifyForms{
+@override
+List<Attribute> get attributes => [
+  FormifyAttribute('email', FormifyType.email),
+  FormifyAttribute('password', FormifyType.password),
+];
+
+@override
+Map<String, List<FormifyRule>> get rules => {
+  'email': [FormifyRule.required],
+  'password': [FormifyRule.required],
+};
+
+@override
+InputDecoration? get inputDecoration => InputDecoration(
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+  ),
+focusedBorder: OutlineInputBorder(
+  borderSide: const BorderSide(color: Colors.purple),
+  borderRadius: BorderRadius.circular(10),
+),
+contentPadding: const EdgeInsets.symmetric(
+  horizontal: 16,
+  vertical: 16
+)
+);
+
+@override
+  FormifyFormBuilder? get formBuilder => (
+        context,
+        Formify formify,
+        FormifyTextField child,
+      ) {
+        if (formify.attribute == 'password') {
+          return child.copyWith(
+            suffixIcon: IconButton(
+              onPressed: formify.toggleObscureText,
+              icon: formify.obscureText
+                  ? const Icon(Icons.visibility)
+                  : const Icon(Icons.visibility_off),
+            ),
+          );
+        }
+        return child;
+      };
+
+    
+
 }
